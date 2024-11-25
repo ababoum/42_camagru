c70d16d981f561aee1c99fd43b6205c565ed92bd
ababoum <ababou237@hotmail.com>
2024-11-25 17:41:36 +0000

    25-nov-2024 * solving DB connection issue

--- srcs/requirements/php/website/templates/layout.php
+++ srcs/requirements/php/website/templates/layout.php
@@ -7,6 +7,7 @@
 </title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 <link rel="stylesheet" href="style.css">
+<link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
 </head>
 
 <div id="page-content">
